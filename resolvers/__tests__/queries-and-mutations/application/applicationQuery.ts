c56aa1870278f8e02b2a8f29a1987154fe701ddb
Marvin Binder <marvin.binder@brickmakers.de>
2021-09-10 13:15:52 +0200

    [GG-301] Integration test logging for failing tests without apparent reason

--- resolvers/__tests__/queries-and-mutations/application/applicationQuery.ts
+++ resolvers/__tests__/queries-and-mutations/application/applicationQuery.ts
@@ -3,7 +3,14 @@ import gql from "graphql-tag";
 export const APPLICATION_QUERY = gql`
   query application($id: String!) {
     application(id: $id) {
-      id
+      success
+      problems {
+        code
+        message
+      }
+      result {
+        id
+      }
     }
   }
 `;
