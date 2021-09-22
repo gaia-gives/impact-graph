b62789155d652deab6e03f05e1f31df090017d7a
Marvin Binder <marvin.binder@brickmakers.de>
2021-09-22 09:10:35 +0200

    [No-Issue] Remove unused resolvers

--- resolvers/index.ts
+++ resolvers/index.ts
@@ -2,14 +2,10 @@ import { ImpactLocationResolver } from "./impactLocationResolver";
 import { UserResolver } from "./userResolver";
 import { ProjectResolver } from "./projectResolver";
 import { OrganisationResolver } from "./organisationResolver";
-import { NotificationResolver } from "./notificationResolver";
 import { LoginResolver } from "./loginResolver";
 import { RegisterResolver } from "./registerResolver";
 import { MeResolver } from "./meResolver";
-import { BankAccountResolver } from "./bankAccountResolver";
-import { UploadResolver } from "./uploadResolver";
 import { CategoryResolver } from "./categoryResolver";
-import { DonationResolver } from "./donationResolver";
 import { NewPasswordResolver } from "./newPasswordResolver";
 import { ConfirmUserResolver } from "./confirmUserResolver";
 import { ApplicationResolver } from "./applicationResolver";
@@ -21,14 +17,10 @@ const resolvers: NonEmptyArray<Function> = [
   UserResolver,
   ProjectResolver,
   OrganisationResolver,
-  NotificationResolver,
   LoginResolver,
   RegisterResolver,
   MeResolver,
-  BankAccountResolver,
-  UploadResolver,
   CategoryResolver,
-  DonationResolver,
   NewPasswordResolver,
   ConfirmUserResolver,
   ApplicationResolver,
