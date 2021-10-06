11e7164fa0b9c64a05762f3b27363bb0f26f598c
Marvin Binder <marvin.binder@brickmakers.de>
2021-10-06 06:51:35 +0200

    [GG-189] Add sendgrid package and adapt function for sending mails

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -1,15 +1,17 @@
 import * as nodemailer from "nodemailer";
 import config from "../config";
+import sgMail from "@sendgrid/mail";
+import { MailContent } from "@sendgrid/helpers/classes/mail";
 
-interface MailOptions {
+interface MailOptions extends MailContent {
   from: string; // sender address
   to: string; // list of receivers
   subject: string; // Subject line
-  text?: string; // plain text body
+  text: string; // plain text body
   html?: string;
 }
 
-export async function sendEmail(options: MailOptions) {
+function sendMailViaNodeMailer(options: MailOptions) {
   const transporter = nodemailer.createTransport({
     host: config.get("MAIL_HOST"),
     port: config.get("MAIL_PORT"),
@@ -18,10 +20,28 @@ export async function sendEmail(options: MailOptions) {
       pass: config.get("MAIL_PASS"),
     },
   });
-
+  
   try {
     transporter.sendMail(options);
   } catch (error) {
     console.log(error);
+  }
+}
+
+function sendMailViaSendGrid(options: MailOptions) {
+  sgMail.setApiKey(config.get("SENDGRID_API_KEY"));
+  try {
+    sgMail.send(options);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function sendEmail(options: MailOptions) {
+  const useSendgrid = config.get("SENDGRID_USE");
+  if (useSendgrid) {
+    sendMailViaSendGrid(options);
+  } else {
+    sendMailViaNodeMailer(options);
   }
 }
